a2b66a429d3f188c7bde1d6a9742e428b6135958
Diana <120332071+DidishechkaM@users.noreply.github.com>
2023-02-16 16:51:27 +0300

    Update Main.java

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -2,6 +2,6 @@ package ru.dianamaksimowa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!i am conflict, sorry99999999988");
+        System.out.println("!i am conflict, sorry");
     }
 }
