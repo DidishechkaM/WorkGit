a6c487526b8d700bcd3d92b1829a4daa5222631d
Diana <120332071+DidishechkaM@users.noreply.github.com>
2023-02-16 16:20:02 +0300

    Update Main.java

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -2,6 +2,6 @@ package ru.dianamaksimowa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("New branch!");
+        System.out.println("!i am conflict, sorry");
     }
 }
