9091d261b4820f453593b592665c284717f4fdaa
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-02-17 13:17:51 +0300

    my work

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -3,7 +3,7 @@ package ru.dianamaksimowa;
 public class Main {
     public static void main(String[] args) {
  today
-        System.out.println("I work long time!");
+        System.out.println("I wait changes");
 
     }
 }
