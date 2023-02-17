ba514a00bd505480b50df709de6dc3a2e8bf3464
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-02-17 12:41:20 +0300

    my changes

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -2,6 +2,6 @@ package ru.dianamaksimowa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("problem");
+        System.out.println("I work long time");
     }
 }
