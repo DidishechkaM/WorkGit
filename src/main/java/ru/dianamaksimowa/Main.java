a66f182f3f587c57a039dc1cd81f34c22d77750a
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-02-17 13:22:23 +0300

    Hello branch

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -1,9 +1,11 @@
 package ru.dianamaksimowa;
 
 public class Main {
+    private static Object today;
+
     public static void main(String[] args) {
  today
-        System.out.println("I wait changes");
+        System.out.println("Hello");
 
     }
 }
