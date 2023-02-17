277c016625e7e6c5007080d66beb79b4bc67df39
Diana <120332071+DidishechkaM@users.noreply.github.com>
2023-02-17 12:41:27 +0300

    Update Main.java

--- src/main/java/ru/dianamaksimowa/Main.java
+++ src/main/java/ru/dianamaksimowa/Main.java
@@ -2,6 +2,6 @@ package ru.dianamaksimowa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("problem");
+        System.out.println("i know what i mean");
     }
 }
